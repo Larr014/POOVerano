3eac837bd66eb8ebdb98f1629a9a7a0fdbb8bd97
Larr014 <larr014@gmail.com>
2020-01-20 14:03:58 -0300

    Add files via upload

---
+++ EscuelitaGUI/src/DAO/DAO.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Modelo.Asignatura;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author larr0
+ */
+public class DAO {
+    private ArrayList<Asignatura> listaAsignaturas = new ArrayList<>();
+
+    public ArrayList<Asignatura> getListaAsignaturas() {
+        return listaAsignaturas;
+    }
+
+    public void setListaAsignaturas(ArrayList<Asignatura> listaAsignaturas) {
+        this.listaAsignaturas = listaAsignaturas;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
